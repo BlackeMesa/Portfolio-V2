dea3f704111a02de59350c734a68ce2adedbe8c6
guillaume Duplessis <guillaumeduplessis@yahoo.fr>
2024-10-02 10:33:29 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,67 @@
+# Duplessis Guillaume Portfolio
+
+Welcome to the source code repository for Duplessis Guillaume's personal portfolio. This Next.js application showcases my projects, skills, and professional experiences. It’s designed to highlight my capabilities in web development, particularly using modern technologies like React and Next.js.
+
+## Features
+
+- **Interactive UI**: A clean, responsive design that works on all devices and screens.
+- **Dynamic Content**: Portfolio pieces and professional details dynamically loaded from structured data.
+- **SEO Optimized**: Implemented using Next.js for improved search engine visibility.
+
+## Getting Started
+
+Follow these instructions to get a copy of the portfolio running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+You'll need to have the following installed to run the project locally:
+
+- Node.js
+- npm (Comes with Node.js)
+
+You can download and install them from [Node.js official website](https://nodejs.org/).
+
+### Installation
+
+1. **Clone the repository**
+
+
+git clone https://github.com/yourgithubusername/duplessis-guillaume-portfolio.git
+cd duplessis-guillaume-portfolio
+Install dependencies
+bash
+Copier le code
+npm install
+Run the development server
+bash
+Copier le code
+npm run dev
+Open http://localhost:3000 to view the portfolio in the browser.
+
+Testing
+To run the automated tests for this project, execute:
+
+bash
+Copier le code
+npm test
+Deployment
+To deploy the portfolio on a live system, execute:
+
+bash
+Copier le code
+npm run build
+npm start
+Built With
+Next.js - The React framework for production
+React - A JavaScript library for building user interfaces
+Tailwind CSS - A utility-first CSS framework for rapid UI development
+Contributing
+Contributions are what make the open-source community such an amazing place to learn, inspire, and create. Any contributions you make are greatly appreciated. Please refer to the contributing guidelines for more information.
+
+Versioning
+We use SemVer for versioning. For the versions available, see the tags on this repository.
+
+Author
+Duplessis Guillaume
+License
+This project is licensed under the MIT License - see the LICENSE.md file for details.
---
+++ data/index.ts
@@ -0,0 +1,225 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Constantly learning and improving",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Thunder Stock, the stock market app",
+    smallTitle: "Thunder Stock",
+    des: "Discover the stock TCG market with Thunder Stock. Analyze stocks, track your portfolio, and stay informed with real-time updates.",
+    img: "/mockup-thunder.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/sharp.png", "/net.png"],
+    link: "https://app.thunder-stock.com/",
+  },
+  {
+    id: 2,
+    title: "Siborg Ads - The Web3 Monetization Platform",
+    smallTitle: "Siborg Ads",
+    des: "Siborg Ads is a decentralized advertising platform built on the Ethereum blockchain. ",
+    img: "/mockup-siborg.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/graph.png", "/thirdweb.jpg"],
+    link: "https://dsponsor-app.siborg.io/",
+  },
+  {
+    id: 3,
+    title: "HooBank - The Next Generation Banking Platform",
+    smallTitle: "HooBank",
+    des: "HooBank is a next-generation banking platform that offers a range of financial services, including savings accounts, checking accounts, and investment options.",
+    img: "/mockup-hoobank.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg"],
+    link: "https://hoobank-react-app.netlify.app/",
+  },
+  {
+    id: 4,
+    title: "MyTube - The YouTube Clone",
+    smallTitle: "MyTube",
+    des: "MyTube is a YouTube clone that allows users to watch videos, upload videos, and interact with other users.",
+    img: "/mockup-mytube.png",
+    iconLists: ["/re.svg", "/redux.png", "/material.png"],
+    link: "https://mytubedg.netlify.app/",
+  },
+  {
+    id: 5,
+    title: "WatchTower - The Coin Tracker",
+    smallTitle: "WatchTower",
+    des: "WatchTower is a coin tracker that allows users to track the price of their favorite coins.",
+    img: "/mockup-watchtower.png",
+    iconLists: ["/re.svg", "/redux.png"],
+    link: "https://crypto-watchtower.netlify.app/",
+  },
+  {
+    id: 6,
+    title: "My Github Portfolio",
+    smallTitle: "My Github",
+    des: "My github portfolio is a portfolio that allows users to view my github projects.",
+    img: "/github.png",
+    iconLists: [],
+    link: "https://github.com/BlackeMesa",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Guillaume has been an integral part of our team here at Thunder. His adaptability and skill set have greatly contributed to our success across multiple projects. What stands out most is Guillaume’s ability to quickly understand and respond to new challenges, no matter how complex. He not only adapts to changes but also excels in his role, driving innovation and efficiency. His commitment and expertise have not gone unnoticed, and they continue to make a significant impact on our operations. Guillaume is a true asset to any team",
+    name: "Grégoire Da Costa Adâo ",
+    title: "CEO of Thunder Stock",
+    img: "/grégoire.jpg",
+  },
+  {
+    quote:
+      "Having Guillaume on the Web3 Siborg Ads project was a game-changer for our team. His deep understanding of the technology and the Web3 space was instrumental in driving our project forward. Guillaume's ability to quickly adapt to evolving requirements and his innovative approach to solving problems helped us navigate through challenges seamlessly. His contributions were not only technical but also strategic, making a significant impact on the success of our project. It’s rare to find someone so committed and capable, and I truly value our collaboration.",
+    name: "Anthony Gourraud",
+    title: "Co-founder of Siborg Ads",
+    img: "/anthony.jpg",
+  },
+  {
+    quote:
+      "I had the pleasure of working with Guillaume on the development of our new website, and I couldn't be more pleased with the results. From the initial concept to the final launch, Guillaume demonstrated exceptional professionalism, creativity, and technical expertise. The site not only looks fantastic but also functions seamlessly, exceeding our expectations. Guillaume's attention to detail and proactive approach in solving problems was truly impressive. I highly recommend Guillaume for any web development projects and look forward to collaborating again in the future.",
+    name: "Paul Quentin",
+    title: "Mercurelab co-founder",
+    img: "/paul.jpg",
+  },
+  {
+    quote:
+      "Working alongside Guillaume at Thunder has been a truly rewarding experience. His adaptability and broad skill set stand out consistently. Guillaume embraces every new challenge with a proactive attitude, making him a dependable teammate under all circumstances. His ability to navigate complex projects and deliver results efficiently has not only enhanced our team's productivity but also inspired us all. Guillaume’s contributions are highly valued, and his positive influence is felt throughout our team.",
+    name: "Laurie De Torres",
+    title: "Marketing Manager at Thunder Stock",
+    img: "/laurie.jpg",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "thunder-stock",
+    img: "/thunder.png",
+ 
+  },
+  {
+    id: 2,
+    name: "siborg-ads",
+    img: "/siborg-ads.webp",
+  },
+  {
+    id: 3,
+    name: "Mercure-lab",
+    img: "/mercurelab.svg",
+  
+  },
+
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Lead Full Stack Developer",
+    desc: "Developed and maintained a web-based platform using React, Tailwind, Typescript, .NET, C#, SQL, and Azure. ",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 2,
+    title: "Web3 Frontend Developer",
+    desc: "Designed and developed a web3 platform using Next, Tailwind, Typescript, Thirdweb, GraphQL, and Solidity. ",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a Website for a client, from initial concept to deployment on the web.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+
+  {
+    id: 4,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using Blazor, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/BlackeMesa",
+  },
+ 
+  {
+    id: 2,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/guillaume-duplessis/",
+  },
+];
